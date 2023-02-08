07f488374659c81b1ba20920d5f77e8080826c24
Ehab98 <hoba.reda98@gmail.com>
2023-02-08 14:55:13 +0200

    end

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,27 @@
 from django.http import JsonResponse
-
+import json
 
 def api_home(request , *args , **kwargs):
-    return JsonResponse({"message":"from API_home"})
+    
+    #request -> HttpRequest -> Django
+    
+    #request.body
+    print(request.GET) #url query param
+    print (request.POST) 
+    body = request.body  #byte string JSON data
+    data = {}
+    try:
+        data = json.loads(body) #string of JSON data -> python dict
+    except:
+        pass
+    
+    print (data)
+    # data['headers'] = request.headers  #request.META -> 
+    # print(request.headers)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
 
 
     
