c47ad19b35a3d860b2d620ee2bb455062169348a
Ehab98 <hoba.reda98@gmail.com>
2023-02-08 12:21:05 +0200

    endpoint-API

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,10 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200"
-endpoint = "https://httpbin.org/anything"
-get_response = requests.get(endpoint)
+# endpoint = "https://httpbin.org/status/200"
+#endpoint = "https://httpbin.org/anything"
+endpoint = " http://localhost:8000/" #http://127.0.0.1:8000/
+
+get_response = requests.get(endpoint,json={"query":"hello API"})
 print(get_response.text)
-print (get_response.json())
+# print (get_response.json())
+# print (get_response.status_code)
